6ab95da5c4ffbba7ecf3cefc8a7a492162f614e8
Lakshan Chinthaka <chinthaka21022@gmail.com>
2024-04-08 13:36:08 +0530

    fix 1.0

--- src/Components/FaqSection/FaqSection.jsx
+++ src/Components/FaqSection/FaqSection.jsx
@@ -16,16 +16,16 @@ function FaqSection() {
                 <div className='m-auto lg:mt-5 mb-[50px] h-[325px] w-[334px] md:w-[686px]  lg:w-[896px] lg:h-[149px]  md:h-[207px] mt-[-30px]  p-[24px] gap-[13px] rounded-[7px] bg-[#FAF8FF]'>
 
                     <button type="button"
-                        className="w-[286px] md:w-[584px] text-[20px] md:text-[22px]  lg:w-[848px] font-medium md:text-left  tracking-[0.4px] text-primary flex items-center">
+                        className="w-[286px] md:w-[584px] text-[20px] md:text-[22px]  lg:w-[848px] font-medium md:text-left  tracking-[0.4px] text-primary flex text-left">
                         <span class="mr-4">Lorem ipsum dolor sit amet consectetur. Leo at sit eu libero?</span>
 
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-[14px] h-[28px] md:mt-[-30px] fill-primary  ml-auto shrink-0" viewBox="0 0 124 124">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-[14px] h-[28px]  fill-primary  ml-auto shrink-0" viewBox="0 0 124 124">
                             <path d="M112 50H12C5.4 50 0 55.4 0 62s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12z" data-original="#000000" />
                         </svg>
 
                     </button>
 
-                    <p className='w-[286px] h-[180px] md:w-[638px] xl:h-[76px] lg:w-[848px] text-[#6F6C90] font-normal text-[16px] md:text-[18px] leading-[30px] xl:w-[798px] md:h-[104px] md:text-left text-base'>Lorem ipsum dolor sit amet consectetur. Faucibus commodo suscipit id ipsum. Elementum ultrices nulla faucibus odio est sed aliquam. Sapien massa morbi risus sagittis tortor integer..</p>
+                    <p className='w-[286px] h-[180px] md:w-[638px] xl:h-[76px] lg:w-[848px] text-[#6F6C90] font-normal text-[16px] md:text-[18px] leading-[30px] xl:w-[798px] md:h-[104px] text-left text-base'>Lorem ipsum dolor sit amet consectetur. Faucibus commodo suscipit id ipsum. Elementum ultrices nulla faucibus odio est sed aliquam. Sapien massa morbi risus sagittis tortor integer..</p>
 
                 </div>
 
@@ -33,7 +33,7 @@ function FaqSection() {
                 <div className="bg-[#FAF8FF] lg:mt-[100px] lg:w-[896px] lg:h-[76px] md:w-[686px] md:h-[104px] w-[334px] p-[24px] h-[160px] rounded-[7px] md:mt-20 mt-[135px] m-auto ">
 
                     <button type="button"
-                        className="w-full text-[22px]  font-medium   tracking-[0.4px] text-black flex items-center">
+                        className="w-full text-[22px]  font-medium   tracking-[0.4px] text-black flex text-left">
                         <span class="mr-4">Lorem ipsum dolor sit amet consectetur. Leo at sit eu libero?</span>
 
                         <svg xmlns="http://www.w3.org/2000/svg" className="w-[14px] h-[28px]  ml-auto shrink-0" viewBox="0 0 42 42">
@@ -47,7 +47,7 @@ function FaqSection() {
                 <div className="bg-[#FAF8FF] lg:mt-[100px] lg:w-[896px] lg:h-[76px] md:w-[686px] md:h-[104px] w-[334px] p-[24px] h-[160px] rounded-[7px] md:mt-20 mt-[135px] m-auto ">
 
                     <button type="button"
-                        className="w-full text-[22px]  font-medium   tracking-[0.4px] text-black flex items-center">
+                        className="w-full text-[22px]  font-medium   tracking-[0.4px] text-black flex text-left">
                         <span class="mr-4">Lorem ipsum dolor sit amet consectetur. Leo at sit eu libero?</span>
 
                         <svg xmlns="http://www.w3.org/2000/svg" className="w-[14px] h-[28px]  ml-auto shrink-0" viewBox="0 0 42 42">
